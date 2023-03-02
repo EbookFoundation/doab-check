ca3c0e4fe5eaec8b7ee0391a63ffa72c23f5f0ee
eric <eric@hellman.net>
2023-03-02 17:40:34 -0500

    basic UI!

---
+++ doab_check/views.py
@@ -0,0 +1,31 @@
+"""doab_check views
+"""
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Link
+
+
+class ProvidersView(generic.TemplateView):
+    template_name = 'providers.html'
+
+    def get_context_data(self, **kwargs):
+        providers = Link.objects.order_by('provider').values('provider').distinct()
+        for provider in providers:
+            provider['link_count'] = Link.objects.filter(provider=provider['provider']).count()
+        return {'provider_list': providers}
+
+
+class ProviderView(generic.TemplateView):
+    template_name = 'provider.html'
+
+    def get_context_data(self, **kwargs):
+        prov = kwargs['provider']
+        provider = {'provider': prov}
+        provider_links = Link.objects.filter(provider=prov)
+        provider['link_count'] = provider_links.count()
+        
+        return {'provider': provider, 'links': provider_links}
