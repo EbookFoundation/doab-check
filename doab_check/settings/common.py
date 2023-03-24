5defebdd885e7c7ef2998d6aa5519f6b6cbc2171
eric <eric@hellman.net>
2023-03-24 13:19:19 -0400

    organize settings, setup

--- doab_check/settings/common.py
+++ doab_check/settings/common.py
@@ -14,19 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@5jch%4&#^6%r0t!p#n+24mfb)+f#*=s8c7hqbn&htu&pd4ej'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+print(BASE_DIR)
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,18 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'doab_check.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'doabcheck',
-        'USER': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5433',    }
-}
 
 
 # Password validation
--- doab_check/urls.py
+++ doab_check/urls.py
@@ -2,10 +2,12 @@
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('providers/', views.ProvidersView.as_view(), name='providers'),
     path('providers/<str:provider>/', views.ProviderView.as_view(), name='provider'),
