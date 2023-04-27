1e6bc87ea5caa2c77a5b87352d977eea500a8a2e
eric <eric@hellman.net>
2023-04-27 17:06:05 -0400

    add problems page, summary on home

--- doab_check/urls.py
+++ doab_check/urls.py
@@ -7,8 +7,9 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', views.HomepageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
+    path('problems/', views.ProblemsView.as_view(), name='problems'),
     path('providers/', views.ProvidersView.as_view(), name='providers'),
     path('providers/<str:provider>/', views.ProviderView.as_view(), name='provider'),
     path('publishers/', views.PublishersView.as_view(), name='publishers'),
