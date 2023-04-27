7d1c1f4e805578d60d92218e61eb4e48dcecc560
eric <eric@hellman.net>
2023-04-27 17:05:29 -0400

    add result summaries

--- doab_check/views.py
+++ doab_check/views.py
@@ -1,12 +1,37 @@
 """doab_check views
 """
 
+from django.db.models import Count
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
 from .models import Item, Link
+
+
+class HomepageView(generic.TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        codes = Link.objects.filter(recent_check__isnull=False).order_by('-recent_check__return_code').values(
+            'recent_check__return_code').distinct()
+        for code in codes:
+            code['count'] = Link.objects.filter(
+                recent_check__return_code=code['recent_check__return_code'],
+            ).distinct().count()
+        return {'codes': codes}
+
+
+class ProblemsView(generic.TemplateView):
+    template_name = 'problems.html'
+
+    def get_context_data(self, **kwargs):
+        problems = Link.objects.exclude(
+            recent_check__return_code__exact=200).exclude(recent_check__isnull=True
+        ).order_by(
+            '-recent_check__return_code', 'provider')
+        return {'problem_list': problems}
 
 
 class ProvidersView(generic.TemplateView):
@@ -25,10 +50,16 @@ class ProviderView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         prov = kwargs['provider']
         provider = {'provider': prov}
-        provider_links = Link.objects.filter(provider=prov, live=True)
+        provider_links = Link.objects.filter(provider=prov, live=True, recent_check__isnull=False)
         provider['link_count'] = provider_links.count()
+        codes = provider_links.order_by('-recent_check__return_code').values(
+            'recent_check__return_code').distinct()
+        for code in codes:
+            code['count'] = provider_links.filter(
+                recent_check__return_code=code['recent_check__return_code'],
+            ).distinct().count()
         
-        return {'provider': provider, 'links': provider_links}
+        return {'provider': provider, 'links': provider_links, 'codes': codes}
 
 class PublishersView(generic.TemplateView):
     template_name = 'publishers.html'
@@ -46,12 +77,21 @@ class PublisherView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         pub = kwargs['publisher']
         publisher = {'publisher': pub}
-        publisher_items = Item.objects.filter(publisher_name=pub, status=1)
-        publisher['link_count'] = publisher_items.count()
         if pub == '*** no publisher name ***':
             pub = ''
         publisher_items = Item.objects.filter(
             publisher_name=pub, status=1,
         )
+        publisher['item_count'] = publisher_items.count()
+        publisher_items = publisher_items.filter(links__recent_check__isnull=False).distinct()
         
-        return {'publisher': publisher, 'items': publisher_items}
+        codes = publisher_items.order_by('-links__recent_check__return_code').values(
+            'links__recent_check__return_code').distinct()
+        for code in codes:
+            code['count'] = Link.objects.filter(live=True,
+                recent_check__return_code=code['links__recent_check__return_code'],
+                items__publisher_name=pub).distinct().count()
+        
+        
+        
+        return {'publisher': publisher, 'items': publisher_items, 'codes': codes}
