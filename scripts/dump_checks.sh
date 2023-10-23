20cae92a30c7d2a10b1da935cb60010e0420979e
eric <eric@hellman.net>
2023-10-23 13:39:32 -0400

    fix the script

--- scripts/dump_checks.sh
+++ scripts/dump_checks.sh
@@ -4,4 +4,4 @@
 cd /home/ubuntu/doab-check
 /home/ubuntu/.local/bin/pipenv run python manage.py dump_checks >> dump_checks.log
 gzip dump_checks.csv
-mv dump_checks.zip static/dump_checks.zip
+mv dump_checks.zip static/dump_checks.csv.zip
